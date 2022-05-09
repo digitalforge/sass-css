bcf4f444ed7c10c74a55d96993e1c26a95234269
Jon Doucette <jon@digitalforgedesign.com>
2022-05-08 22:31:12 -0400

    custom css grid using floats

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <Header />
+  <GridTest />
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header"
+import GridTest from "@/components/GridTest"
 
 export default {
-  components: { Header },
+  components: { Header, GridTest },
   setup() {},
 }
 </script>
